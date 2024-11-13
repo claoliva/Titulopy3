210bf4bcbf3e86a01c2a48f46c86d7868cbbd35e
CLAUDIA ALVES DE OLIVEIRA <claoliva2010@gmail.com>
2024-11-13 11:11:55 -0300

    Update Thirdpy.py
    Terceira tentativa

--- Thirdpy.py
+++ Thirdpy.py
@@ -1,9 +1,7 @@
 import streamlit as st
 st.title('Este é o título do app')
-import streamlit as st
 st.head('Este é o subtítulo')
 st.subheader('Este é o terceiro subtítulo')
 st.markdown('Este é texto')
 st.caption('Esta é a a legenda')
 st.cod('x=2021')
-st.latex(r''' a+a r^1+a r^2+a r^3 ''')
