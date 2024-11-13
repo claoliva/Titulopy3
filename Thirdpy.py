988cf296f227603b63b9b77fa1d1ce5252563b55
CLAUDIA ALVES DE OLIVEIRA <claoliva2010@gmail.com>
2024-11-13 11:16:19 -0300

    Update Thirdpy.py
    Quarta tentativa

--- Thirdpy.py
+++ Thirdpy.py
@@ -1,6 +1,6 @@
 import streamlit as st
-st.title('Este é o título do app')
-st.head('Este é o subtítulo')
+st.title('é o título do app')
+st.header('Este é o subtítulo')
 st.subheader('Este é o terceiro subtítulo')
 st.markdown('Este é texto')
 st.caption('Esta é a a legenda')
